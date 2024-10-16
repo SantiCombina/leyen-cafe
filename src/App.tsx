98e663032122fa6142c4a689fd49a58ee6d3d549
SantiCombina <santicombina@gmail.com>
2024-10-16 17:13:33 -0300

    feat(login): Add zustand dependency and implement protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
-import {Route, Routes, useNavigate} from "react-router-dom";
-import {useEffect} from "react";
+import {Route, Routes} from "react-router-dom";
 
+import {Home} from "./views/home";
 import {Login} from "./views/login";
 import {NotFound} from "./views/not-found";
-import {Home} from "./views/home";
-
-import {supabase} from "@/supabase/supabase";
+import {ProtectedRoute} from "./components/protected-routes";
 
 function App() {
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        supabase.auth.onAuthStateChange((event, session) => {
-            if (!session) {
-                navigate("/login");
-            } else {
-                navigate("/");
-            }
-        });
-    }, [navigate]);
-
     return (
         <div className="min-h-[100dvh] flex flex-col justify-center items-center p-6">
             <Routes>
-                <Route element={<Home />} path="/" />
+                <Route element={<ProtectedRoute />}>
+                    <Route element={<Home />} path="/" />
+                </Route>
                 <Route element={<Login />} path="/login" />
                 <Route element={<NotFound />} path="*" />
             </Routes>
---
+++ src/store/login-store.tsx
@@ -0,0 +1,51 @@
+import {create} from "zustand";
+import {Session} from "@supabase/supabase-js";
+
+import {supabase} from "@/supabase/supabase";
+
+interface LoginStore {
+    session: Session | null | undefined;
+    isLogged: boolean;
+    loading: boolean;
+    loginGoogle: () => void;
+    logout: () => void;
+    checkUser: () => void;
+}
+
+export const useLoginStore = create<LoginStore>()((set) => ({
+    session: undefined,
+    isLogged: false,
+    loading: true,
+    checkUser: async () => {
+        supabase.auth.onAuthStateChange((event, session) => {
+            if (event || session) {
+                set({session: session});
+            } else {
+                set({session: null});
+            }
+        });
+    },
+    loginGoogle: async () => {
+        await supabase.auth.signInWithOAuth({
+            provider: "google",
+            options: {
+                queryParams: {prompt: "select_account"},
+                redirectTo: "http://localhost:5173/",
+            },
+        });
+        set({
+            isLogged: true,
+            loading: false,
+        });
+    },
+    logout: async () => {
+        set({
+            loading: true,
+        });
+        await supabase.auth.signOut();
+        set({
+            isLogged: false,
+            loading: false,
+        });
+    },
+}));
