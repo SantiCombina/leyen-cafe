cef110870810728f4a64413c692fec09cde296fe
Santiago Combina <128075924+SantiCombina@users.noreply.github.com>
2024-10-17 19:17:59 -0300

    Update layout.tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,11 +1,8 @@
 import {Outlet} from "react-router-dom";
 
-import {Navbar} from "./navbar";
-
 export function Layout() {
     return (
         <div className="min-h-[100dvh] text-primary bg-background flex flex-col justify-center items-center">
-            <Navbar />
             <Outlet />
         </div>
     );
